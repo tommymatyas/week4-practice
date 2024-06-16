b4e525a1137e92b2033fa9d2f891632a5478775c
Tamas Matyas <matyas_tommy@hotmail.com>
2024-06-16 11:12:45 +0200

    changes

---
+++ script.js
@@ -0,0 +1,35 @@
+const transactions = [
+  {
+    type: "deposit",
+    amount: 20000,
+  },
+  {
+    type: "deposit",
+    amount: 15000,
+  },
+  {
+    type: "withdrawal",
+    amount: 8000,
+  },
+  {
+    type: "deposit",
+    amount: 500000,
+  },
+  {
+    type: "withdrawal",
+    amount: 12000,
+  },
+  {
+    type: "withdrawal",
+    amount: 30000,
+  },
+];
+
+
+ const largestTransaction = () => {
+    let largest = transactions[0];
+
+    for (let i = 0; i <transactions.length, i++) {
+
+    }
+ }
